7e88f4651541579312c33776e88a02ce08123da4
Felix <uxhoe@student.kit.edu>
2025-02-07 17:40:03 +0100

    added wall clock incnum

---
+++ plots/code/wall_clock_incnum_preproc.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+x1 = 1
+x2 = 5
+x3 = 10
+x4 = 25
+x5 = 50
+x6 = 75
+x7 = 100
+
+# Wall Clock Time for Increasing Number of of RDPF-Triples
+
+df_mpz_64 = pd.DataFrame({
+    "x":[x1,x2,x3,x4,x5,x6,x7],
+    "y":[2.1,3.4,6.7,13.07,26.3,39.72,54.83],
+    "Approach": "gmp_mpz_class 64 bit"
+})
+
+df_mpz_128 = pd.DataFrame({
+    "x":[x1,x2,x3,x4,x5,x6,x7],
+    "y":[4.1,5.2,8.5,19.2,36.2,43.12,69.1],
+    "Approach":"gmp_mpz_class 128 bit"
+})
+
+df_mpz_256 = pd.DataFrame({
+    "x":[x1,x2,x3,x4,x5,x6,x7],
+    "y":[8.1,9.2,12.13,22.3,37.3,53.2,86.2],
+    "Approach":"gmp_mpz_class 256 bit"
+})
+
+df_base = pd.DataFrame({
+    "x":[x1,x2,x3,x4,x5,x6,x7],
+    "y":[0.044,0.05,0.062,0.06,0.15,0.26,0.23],
+    "Approach":"base"
+})
+
+df_new_input_type = pd.DataFrame({
+    "x":[x1,x2,x3,x4,x5,x6,x7],
+    "y":[0.05,0.049,0.061,0.063,0.11,0.28,0.25],
+    "Approach":"new_input_type"
+})
+
+plt.figure(figsize=(8,5))
+
+sns.lineplot(data=df_mpz_64, x="x", y="y", hue="Approach", marker="*", linestyle="dashed", palette=["#ff6666"])
+sns.lineplot(data=df_mpz_128, x="x", y="y", hue="Approach", marker="*", linestyle="dashed", palette=["#cc0000"])
+sns.lineplot(data=df_mpz_256, x="x", y="y", hue="Approach", marker="*", linestyle="dashed", palette=["#800000"])
+
+sns.lineplot(data=df_base, x="x", y="y", hue="Approach", marker=".", linestyle="dotted", palette=["Green"])
+
+sns.lineplot(data=df_new_input_type, x="x", y="y", hue="Approach", marker="o", linestyle="dashed", palette=["#66b3ff"])
+
+plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
+sns.set_style("ticks")
+plt.grid(axis="y")
+plt.grid(axis="x")
+plt.xlabel("Number of generated RDPF-Triples")
+plt.ylabel("Wall Clock Time (s)")
+plt.savefig("wall_clock_incnum_preproc.png", bbox_inches="tight")
--- plots/code/wall_clock_time_preproc.py
+++ plots/code/wall_clock_time_preproc.py
@@ -9,74 +9,52 @@ x4 = 15
 x5 = 20
 
 # Wall Clock Time Preproc
-y1 = 1.579
-y2 = 1.6966
-y3 = 1.7308
-y4 = 1.8307
-y5 = 3.58326
 
-df0 = pd.DataFrame({
+df_new_input_type = pd.DataFrame({
     "x":[x1,x2,x3,x4, x5],
-    "y":[y1,y2,y3,y4, y5],
+    "y":[0.1,0.1,0.124,0.225,0.421],
     "Approach": "new_input_type"
 })
 
 df_base = pd.DataFrame({
     "x":[x1,x2,x3,x4, x5],
-    "y":[y1,y2,y3,y4, y5],
+    "y":[0.1,0.1,0.124,0.225,0.421],
     "Approach": "base"
 })
 
-y1 = 1.5568
-y2 = 1.5957
-y3 = 1.6233
-y4 = 2.1463
-y5 = 4.0214
-
-df3 = pd.DataFrame({
+df_mpz_class_64 = pd.DataFrame({
     "x":[x1,x2,x3,x4, x5],
-    "y":[y1,y2,y3,y4, y5],
+    "y":[0.8,1.21,2.486,24.452,1029.619],
     "Approach": "gmp_mpz_class 64 bit"
 })
 
-y1 = 1.9621
-y2 = 1.98335
-y3 = 2.33319
-y4 = 2.495239
-y5 = 4.96041
-
-df4 = pd.DataFrame({
+df_mpz_class_128 = pd.DataFrame({
     "x":[x1,x2,x3,x4, x5],
-    "y":[y1,y2,y3,y4, y5],
+    "y":[0.902,1.449,2.989,26.836,1192.027],
     "Approach": "gmp_mpz_class 128 bit"
 })
 
-y1 = 2.4118
-y2 = 2.4395
-y3 = 2.675159
-y4 = 2.833388
-y5 = 5.321
-
-df5 = pd.DataFrame({
+df_mpz_class_256 = pd.DataFrame({
     "x":[x1,x2,x3,x4, x5],
-    "y":[y1,y2,y3,y4, y5],
+    "y":[0.903,1.341,2.964,27.530,1283.07],
     "Approach": "gmp_mpz_class 256 bit"
 })
 
 plt.figure(figsize=(8, 5))
 
-sns.lineplot(data=df0, x="x", y="y", hue="Approach", marker="o", linestyle="dashed", palette=["#66b3ff"])
+sns.lineplot(data=df_new_input_type, x="x", y="y", hue="Approach", marker="o", linestyle="dashed", palette=["#66b3ff"])
 
-sns.lineplot(data=df3, x="x", y="y", hue="Approach", marker="*", linestyle="dashed", palette=["#ff6666"])
-sns.lineplot(data=df4, x="x", y="y", hue="Approach", marker="*", linestyle="dashed", palette=["#cc0000"])
-sns.lineplot(data=df5, x="x", y="y", hue="Approach", marker="*", linestyle="dashed", palette=["#800000"])
+sns.lineplot(data=df_mpz_class_64, x="x", y="y", hue="Approach", marker="*", linestyle="dashed", palette=["#ff6666"])
+sns.lineplot(data=df_mpz_class_128, x="x", y="y", hue="Approach", marker="*", linestyle="dashed", palette=["#cc0000"])
+sns.lineplot(data=df_mpz_class_256, x="x", y="y", hue="Approach", marker="*", linestyle="dashed", palette=["#800000"])
 
 sns.lineplot(data=df_base, x="x", y="y", hue="Approach", marker=".", linestyle="dotted", palette=["Green"])
 
+plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
 sns.set_style("ticks")
 plt.grid(axis="y")
 plt.grid(axis="x")
 plt.xticks(range(3, 21, 1))
 plt.xlabel("Depth")
 plt.ylabel("Wall Clock Time (s)")
-plt.savefig("time_preproc.png")
+plt.savefig("wall_clock_preproc.png", bbox_inches="tight")
