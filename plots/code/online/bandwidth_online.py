be2fa6a1db59055085e03595b6db092f3221625f
Felix <uxhoe@student.kit.edu>
2025-02-10 16:49:49 +0100

    eigener ordner für preproc

---
+++ plots/code/online/bandwidth_online.py
@@ -0,0 +1,102 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+x1 = 1500
+x2 = 1250
+x3 = 1000
+x4 = 750
+x5 = 100
+x6 = 10
+x7 = 5
+
+# Wallclock Time for decreasing bandwidth, constant latency (1ms),
+# constant depth (15) and constant number of retreived itemas (1)
+
+##### READ ####
+
+df_main = pd.DataFrame({
+    "x": [x1,x2,x3,x4,x5],
+    "y":,
+    "Approach"
+})
+
+df_mpz_64 = pd.DataFrame({
+    "x": [x1,x2,x3,x4,x5],
+    "y":,
+    "Approach"
+})
+
+df_mpz_128 = pd.DataFrame({
+    "x": [x1,x2,x3,x4,x5],
+    "y":,
+    "Approach"
+})
+
+df_mpz_256 = pd.DataFrame({
+    "x": [x1,x2,x3,x4,x5],
+    "y":,
+    "Approach"
+})
+
+df_nit_64 = pd.DataFrame({
+    "x": [x1,x2,x3,x4,x5],
+    "y":,
+    "Approach"
+})
+
+df_nit_128 = pd.DataFrame({
+    "x": [x1,x2,x3,x4,x5],
+    "y":,
+    "Approach"
+})
+
+df_nit_256 = pd.DataFrame({
+    "x": [x1,x2,x3,x4,x5],
+    "y":,
+    "Approach"
+})
+
+##### UPDATE #####
+
+df_main = pd.DataFrame({
+    "x": [x1,x2,x3,x4,x5],
+    "y":,
+    "Approach"
+})
+
+df_mpz_64 = pd.DataFrame({
+    "x": [x1,x2,x3,x4,x5],
+    "y":,
+    "Approach"
+})
+
+df_mpz_128 = pd.DataFrame({
+    "x": [x1,x2,x3,x4,x5],
+    "y":,
+    "Approach"
+})
+
+df_mpz_256 = pd.DataFrame({
+    "x": [x1,x2,x3,x4,x5],
+    "y":,
+    "Approach"
+})
+
+df_nit_64 = pd.DataFrame({
+    "x": [x1,x2,x3,x4,x5],
+    "y":,
+    "Approach"
+})
+
+df_nit_128 = pd.DataFrame({
+    "x": [x1,x2,x3,x4,x5],
+    "y":,
+    "Approach"
+})
+
+df_nit_256 = pd.DataFrame({
+    "x": [x1,x2,x3,x4,x5],
+    "y":,
+    "Approach"
+})
---
+++ plots/code/online/latency_online.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+x1 = 1
+x2 = 2.5
+x3 = 5
+x4 = 7.5
+x5 = 10
+x6 = 50
+x7 = 75
+x8 = 100
+
+# Wallclock time for decreasing latency (x), constant bandwidth (100mbit),
+# constant depth (15), and constant number of retrieved / updated items (1)
+# 50 rdpf triples have been generated before
+
+df_main = pd.DataFrame({
+    "x": [x1,x2,x3,x4,x5],
+    "y":,
+    "Approach"
+})
+
+df_mpz_64 = pd.DataFrame({
+    "x": [x1,x2,x3,x4,x5],
+    "y":,
+    "Approach"
+})
+
+df_mpz_128 = pd.DataFrame({
+    "x": [x1,x2,x3,x4,x5],
+    "y":,
+    "Approach"
+})
+
+df_mpz_256 = pd.DataFrame({
+    "x": [x1,x2,x3,x4,x5],
+    "y":,
+    "Approach"
+})
+
+df_nit_64 = pd.DataFrame({
+    "x": [x1,x2,x3,x4,x5],
+    "y":,
+    "Approach"
+})
+
+df_nit_128 = pd.DataFrame({
+    "x": [x1,x2,x3,x4,x5],
+    "y":,
+    "Approach"
+})
+
+df_nit_256 = pd.DataFrame({
+    "x": [x1,x2,x3,x4,x5],
+    "y":,
+    "Approach"
+})
